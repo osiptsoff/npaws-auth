0a77d17469a0b84a9fab66c05dba69698eeb5f93
osiptsoff <osipczovnikita@gmail.com>
2025-05-29 02:05:32 +0300

    hide confusing warning

--- src/main/java/ru/osiptsoff/npaws_auth/NpawsAuthApplication.java
+++ src/main/java/ru/osiptsoff/npaws_auth/NpawsAuthApplication.java
@@ -2,8 +2,9 @@ package ru.osiptsoff.npaws_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 public class NpawsAuthApplication {
 
 	public static void main(String[] args) {
